e49b73bbbee1f7300f0b2dc5eee17b72b43a300d
Mir HB Rahman <ahadcr0@gmail.com>
2019-04-18 15:00:59 +0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,15 @@ let socket = io();
 socket.on('connect',function(){
   console.log('Connected to server');
 
-  socket.emit('createMessage',{
-    from: 'tada',
-    text: 'dddd'
-  });
+  // socket.emit('createMessage',{
+  //   from: 'john',
+  //   text: 'This is test'
+  // });
 });
 
 socket.on('newMessage', function(message){
-  console.log('New message', message);
+  console.log('newMessage', message);
 });
-
-
 
 socket.on('disconnect', function(){
   console.log('Disconnected form server');
