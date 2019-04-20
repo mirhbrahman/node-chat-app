0944d19b01226294662b845b8b83ca58c33ef9ea
Mir HB Rahman <ahadcr0@gmail.com>
2019-04-20 20:32:11 +0600

    Scrool to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 let socket = io();
+
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect',function(){
   console.log('Connected to server');
 });
@@ -12,6 +29,7 @@ socket.on('newMessage', function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom ();
  });
 
 socket.on('newLocationMessage', function(message){
@@ -23,6 +41,7 @@ socket.on('newLocationMessage', function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom ();
 });
 
 socket.on('disconnect', function(){
