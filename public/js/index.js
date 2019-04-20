b4cc99347cde687c7b3ab27fbd451c43e091f339
Mir HB Rahman <ahadcr0@gmail.com>
2019-04-20 18:24:43 +0600

    Add css to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,7 +31,7 @@ jQuery('#message-form').on('submit', function(e){
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function(){
-    console.log('Message send');
+    jQuery('[name=message]').val('');
   });
 });
 
@@ -40,12 +40,17 @@ locationButton.on('click', function(){
   if(!navigator.geolocation){
     return alert('Geolocation not support by your browser.');
   }
+
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
     });
   }, function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fatch location');
   });
 });
